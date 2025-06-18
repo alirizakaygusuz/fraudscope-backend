fdac31ceecc44c73460d0047ec3e28783dd84125
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2025-06-18 14:31:28 +0300

    Initial commit: Base project setup, dependencies and .gitignore added

---
+++ src/main/java/com/finscope/fraudscope/FraudscopeApplication.java
@@ -0,0 +1,13 @@
+package com.finscope.fraudscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudscopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finscope/fraudscope/FraudscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finscope.fraudscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
