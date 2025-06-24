cf276355189005ccce091665ea35dfabd10a372c
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2025-06-24 23:10:02 +0300

    feat(common): add generic ApiResponse structure with status enum, error model and base controller

---
+++ src/main/java/com/finscope/fraudscope/common/response/ResponseStatus.java
@@ -0,0 +1,24 @@
+package com.finscope.fraudscope.common.response;
+
+public enum ResponseStatus	{
+    OK(200),
+    CREATED(201),
+    NO_CONTENT(204),
+    BAD_REQUEST(400),
+    UNAUTHORIZED(401),
+    FORBIDDEN(403),
+    NOT_FOUND(404),
+    CONFLICT(409),
+    UNPROCESSABLE_ENTITY(422),
+    INTERNAL_SERVER_ERROR(500);
+
+    private final int code;
+
+    ResponseStatus	(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
